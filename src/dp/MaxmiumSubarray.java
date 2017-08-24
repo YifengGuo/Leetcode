20d1ecd80f9e3f23e3deee8dc01810f6377bf2eb
YifengGuo <gyf940517@gmail.com>
2017-08-24 18:07:03 -0400

    DP

--- src/dp/MaxmiumSubarray.java
+++ src/dp/MaxmiumSubarray.java
@@ -7,6 +7,11 @@ package dp;
 	For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 	the contiguous subarray [4,-1,2,1] has the largest sum = 6.
  */
+
+/*
+ * time = O(n)
+ * space = O(1)
+ */
 public class MaxmiumSubarray {
 	public int maxSubArray(int[] nums) {
 		if (nums == null || nums.length == 0) {
