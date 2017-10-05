b31754d506f2dadf6fa4858d89de2db50748e660
YifengGuo <gyf940517@gmail.com>
2017-10-05 19:41:12 -0400

    two sum using hashmap

---
+++ src/TopInterviewQuestions/TwoSumReturnIndex.java
@@ -0,0 +1,40 @@
+package TopInterviewQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 
+ * @author yifengguo
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+	You may assume that each input would have exactly one solution, and you may not use the same element twice.
+	
+	Example:
+	Given nums = [2, 7, 11, 15], target = 9,
+	
+	Because nums[0] + nums[1] = 2 + 7 = 9,
+	return [0, 1].
+ */
+/*
+ * time : O(n)
+ * space = O(n)
+ */
+public class TwoSumReturnIndex {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        // 
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                int left = Math.min(i, map.get(target - nums[i]));
+                int right = Math.max(i, map.get(target - nums[i]));
+                return new int[]{left, right};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
