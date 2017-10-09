e1008f7be9148f252da2068523724f5a92af724a
YifengGuo <gyf940517@gmail.com>
2017-10-08 22:18:37 -0400

    recursive and iterative solution with test for offering null to queue, important question

--- src/TopInterviewQuestions/SymmetricBinaryTree.java
+++ src/TopInterviewQuestions/SymmetricBinaryTree.java
@@ -1,4 +1,8 @@
 package TopInterviewQuestions;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 
  * @author guoyifeng
@@ -19,6 +23,12 @@ package TopInterviewQuestions;
 	   3    3
  */
 public class SymmetricBinaryTree {
+	/*
+	 * recursive solution
+	 * design recursion function by thinking each time 
+	 * we are comparing the left.left with right.right  and left.right with rigt.left
+	 * so recursion function should design with two TreeNodes
+	 */
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
@@ -27,15 +37,57 @@ public class SymmetricBinaryTree {
     }
     
     private boolean helper(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
+        if (left == null && right == null) { // base case
             return true;
-        } else if (left != null && right == null) {
-            return false;
-        } else if (left == null && right != null) {
+        } else if (left == null || right == null) { // if one of two nodes is empty, so cannot be possibly symmetric
             return false;
         } else if (left.val != right.val) { // left and right both not null
                 return false;
         }
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
+    
+    /*
+     * iterative solution using queue
+     */
+	public boolean isSymmetricIterative(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root.left);
+		queue.offer(root.right);
+		while (!queue.isEmpty()) {
+			TreeNode left = queue.poll();
+			TreeNode right = queue.poll();
+			if (left == null && right == null) { // left.left <=> right.right or left.right <=> right.left are both null
+				                               // so far the tree is symmetric
+				continue;
+			} else if (left == null || right == null) { // left.left <=> right.right or left.right <=> right.left 
+											          // one node of its pair is null
+											          // so the tree cannot be symmetric
+				return false;
+			} else if (left.val != right.val) { // value of corresponding node is not equal, not symmetric
+				return false;
+			}
+			// if some child TreeNode is null, the queue will offer null and store it
+			queue.offer(left.left);
+			queue.offer(right.right);
+			queue.offer(left.right);
+			queue.offer(right.left);
+		}
+		return true;
+	}
+	/*
+	 * test offer null to queue
+	 */
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(null);
+		q.offer(1);
+		q.offer(2);
+		while (!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+	}
 }
