25d29801afb05b53befb87aebb14e44247f00a1c
YifengGuo <gyf940517@gmail.com>
2017-10-17 13:46:30 -0400

    goldman sachs question7, similar to 2sum, find number of pairs whose sum = k

---
+++ src/GoldmanSachs/SumKPairs.java
@@ -0,0 +1,34 @@
+package GoldmanSachs;
+
+import java.util.Arrays;
+
+public class SumKPairs {
+	public int pairs(int[] nums, int k) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int count = 0;
+		Arrays.sort(nums);
+		int left = 0;
+		int right = nums.length - 1;
+		while (left < right) {
+			if (nums[left] + nums[right] == k) {
+				count++;
+				left++;
+				right--;
+			} else if (nums[left] + nums[right] < k) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = new int[]{4,1,6,12,7,2,8,13,21,5,3,6};
+		int k = 9;
+		int count = new SumKPairs().pairs(nums, k);
+		System.out.println(count);
+	}
+}
