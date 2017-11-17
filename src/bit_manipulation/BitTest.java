dca74c51e4d3b365904f2b0aae97ca253d240995
YifengGuo <gyf940517@gmail.com>
2017-11-17 14:50:46 -0500

    update note and test

--- src/bit_manipulation/BitTest.java
+++ src/bit_manipulation/BitTest.java
@@ -5,8 +5,8 @@ public class BitTest {
 		System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
 		System.out.println(Integer.MAX_VALUE);
 		System.out.println(Integer.MIN_VALUE);
-		int a = 5;
-		System.out.println(a | 1);
+		int a = 4;
+		System.out.println(a ^ 1);
 		int b = -1;
 		// pad 0 on the significant bits
 		System.out.println(String.format("%32s", Integer.toBinaryString(b >>> 3)).replace(' ', '0'));
