87df569d92fdccecc0cd4adb229ebac6e31061b5
Yifeng Guo <gyf940517@gmail.com>
2017-11-24 10:45:53 -0500

    easy direction to generate random 5 using random 7

---
+++ src/ProbabilityAndSampling/Random5UsingRandom7.java
@@ -0,0 +1,24 @@
+package ProbabilityAndSampling;
+/**
+ * 
+ * @author yifengguo
+ * implement a random number generator which can return 0 - 4 with
+ * equal probability using Random7(which return 0 - 6 with equal probability)
+ */
+public class Random5UsingRandom7 {
+	public int Random5() {
+		while (true) {
+			int random = (int)(Math.random() * 7);
+			if (random < 5) {
+				return random;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Random5UsingRandom7 test = new Random5UsingRandom7();
+		for (int i = 0; i < 20; i++) {
+			System.out.print(test.Random5() + " ");
+		}
+	}
+}
