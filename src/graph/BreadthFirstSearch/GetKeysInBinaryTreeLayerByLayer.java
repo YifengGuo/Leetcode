35f57d4d6e8c18958f1a377b38c5765eb75e061b
YifengGuo <gyf940517@gmail.com>
2017-11-28 14:07:04 -0500

    simple

--- src/graph/BreadthFirstSearch/GetKeysInBinaryTreeLayerByLayer.java
+++ src/graph/BreadthFirstSearch/GetKeysInBinaryTreeLayerByLayer.java
@@ -1,4 +1,10 @@
 package graph.BreadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * 
  * @author yifengguo
@@ -40,6 +46,32 @@ package graph.BreadthFirstSearch;
 	
 	    4
  */
+/*
+ * basic idea: bfs
+ */
 public class GetKeysInBinaryTreeLayerByLayer {
-
+	public List<List<Integer>> layerByLayer(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null) {
+			return res;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> level = new ArrayList<>();
+			for (int i = 0; i < size; i++) {
+				TreeNode cur = queue.poll();
+				level.add(cur.val);
+				if (cur.left != null) {
+					queue.offer(cur.left);
+				}
+				if (cur.right != null) {
+					queue.offer(cur.right);
+				}
+			}
+			res.add(level);
+		}
+		return res;
+	}
 }
