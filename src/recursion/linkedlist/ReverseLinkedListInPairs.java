5e185f669152a9fbc67d96390ca17b02c8fe901f
Yifeng Guo <gyf940517@gmail.com>
2018-01-17 13:21:31 -0500

    update

--- src/recursion/linkedlist/ReverseLinkedListInPairs.java
+++ src/recursion/linkedlist/ReverseLinkedListInPairs.java
@@ -28,10 +28,11 @@ public class ReverseLinkedListInPairs {
 		if (head == null || head.next == null) {
 			return head;
 		}
-		ListNode next = head.next;
+		ListNode cur = head;
+		ListNode next = cur.next;
 		ListNode res = reverseInPairs(next.next);
-		next.next = head;
-		head.next = res;
+		next.next = cur;
+		cur.next = res;
 		return next;
 	}
 }
