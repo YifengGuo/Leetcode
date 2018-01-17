bf4d7c574d07cdf9dc200e18496a2daa9233d5fe
Yifeng Guo <gyf940517@gmail.com>
2018-01-17 14:16:48 -0500

    three step practice 2

---
+++ src/recursion/tree/CountNodesInLeftSubtree.java
@@ -0,0 +1,44 @@
+package recursion.tree;
+/**
+ * 
+ * @author yifengguo
+ * count how many nodes belong to current node's left-subtree
+ */
+/*
+ *                     root
+ *                   /     \
+ *                 l1(X)    r1
+ *              /       \ 
+ *             l11(X1)  r12(X2)
+ *           /      \
+ *          l21(X21) r22(X22)
+ */
+public class CountNodesInLeftSubtree {
+	class TreeNode_1 {
+		TreeNode_1 left;
+		TreeNode_1 right;
+		int value;
+		int total_left; // how many nodes belong to its left-subtree
+		public TreeNode_1(int value) {
+			this.value = value;
+			total_left = 0;
+		}
+	}
+	
+	public int countLeftSubtree(TreeNode_1 root) {
+		// base case
+		if (root == null) {
+			return 0;
+		}
+		// step 1
+		int left = countLeftSubtree(root.left);
+		int right = countLeftSubtree(root.right);
+		
+		// step 2 current layer process
+		root.total_left = left; // update current node's total_left field
+		
+		// step 3
+		return left + right + 1; // parent node's sub-tree include current node's left and right and itself
+		
+	}
+}
