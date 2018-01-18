e7ef1217090a165434b7830d3f76fc33a474425c
Yifeng Guo <gyf940517@gmail.com>
2018-01-18 17:38:10 -0500

    any node to any node direct path problem, solved by prefixSum

---
+++ src/recursion/tree/MaximumPathSumBinaryTree3.java
@@ -0,0 +1,82 @@
+package recursion.tree;
+/**
+ * 
+ * @author yifengguo
+  Given a binary tree in which each node contains an integer number. Find the maximum possible subpath 
+  sum(both the starting and ending node of the subpath should be on the same path from root to one of the leaf nodes, 
+  and the subpath is allowed to contain only one node).
+
+	Assumptions
+	
+	The root of given binary tree is not null
+	Examples
+	
+	   -5
+	
+	  /    \
+	
+	2      11
+	
+	     /    \
+	
+	    6     14
+	
+	           /
+	
+	        -3
+	
+	The maximum path sum is 11 + 14 = 25
+	
+	How is the binary tree represented?
+	
+	We use the level order traversal sequence with a special symbol "#" denoting the null node.
+	
+	For Example:
+	
+	The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+	
+	    1
+	
+	  /   \
+	
+	 2     3
+	
+	      /
+	
+	    4
+ */
+/*
+ * basic idea: maintain a prefix_path from root to current node
+ * demo:
+ *                                 10
+ *                               /    \
+ *                             -2     7
+ *                            /   \
+ *                          8     -4
+ *                          
+ *                from root to node 4:
+ *                	prefix_of_path = {10, 2, -4}
+ *                
+ */
+public class MaximumPathSumBinaryTree3 {
+	public int globalMax = Integer.MIN_VALUE;
+	public int maxPathSum(TreeNode root) {
+		if (root == null) {
+			return Integer.MIN_VALUE;
+		}
+		helper(root, 0);
+		return globalMax;
+	}
+	private void helper(TreeNode root, int prefixSum) {
+		// base case
+		if (root == null) {
+			return;
+		}
+		// because it is any node to any node
+		// we need try to update whenever we reach to a new node
+		globalMax = Math.max(globalMax, root.key + prefixSum);
+		
+		helper(root.left, Math.max(prefixSum + root.key, 0)); // update prefixSum and go left
+		helper(root.right, Math.max(prefixSum + root.key, 0)); // update prefixSum and go right
+	}
+}
