f25e254301ab4f8d31f11002a2d50617d8d00f7d
YifengGuo <gyf940517@gmail.com>
2018-03-26 17:04:34 -0400

    update my more readable method

--- src/graph/BreadthFirstSearch/Bipartite.java
+++ src/graph/BreadthFirstSearch/Bipartite.java
@@ -57,58 +57,101 @@ class GraphNode {
 		this.neighbors = new ArrayList<>();
 	}
 }
+
 public class Bipartite {
 	public boolean isBipartite(List<GraphNode> graph) {
-		if (graph == null || graph.isEmpty()) {
+		if (graph == null || graph.size() == 0) {
 			return true;
 		}
-		// use a hash_map to record if a graphnode which belongs
-		// to a certain group is visited or not
-		// key: current graph node, value: the group it belongs to
-		Map<GraphNode, Integer> visited = new HashMap<>();
+		// key: GraphNode, value: groupID
+		Map<GraphNode, Integer> map = new HashMap<>();
 		for (GraphNode node : graph) {
-			// run bfs on each node of graph and check the validate
-			// relationship between expanded node and its neighbors
-			if (!bfs(node, visited)) {
+			if (!bfs(node, map)) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	private boolean bfs(GraphNode node, Map<GraphNode, Integer> visited) {
-		// if we have visited the node, we do not have to run bfs
-		// on this node again, this prunes the duplicate operations
-		if (visited.containsKey(node)) {
+
+	private boolean bfs(GraphNode node, Map<GraphNode, Integer> map) {
+		// if we have run bfs on current node already and the program does not return
+		// then we do not run it again. return true directly
+		if (map.containsKey(node)) {
 			return true;
 		}
-		// put the current node into random group, say, group 0
-		// then all the neighbors of it shall belong to group 1
-		// if the graph is bipartite
-		visited.put(node, 0);
-		// run bfs
-		Queue<GraphNode> queue = new LinkedList<>();
-		queue.offer(node);
-		while (!queue.isEmpty()) {
-			GraphNode cur = queue.poll();
-			for (GraphNode nei : cur.neighbors) {
-				int groupId = visited.get(cur);
-				// the neighbors shall all belong to opposite
-				// group of current node if the graph is bipartite
-				int neiId = groupId == 0 ? 1 : 0;
-				if (!visited.containsKey(nei)) { // if current neighbor of cur has not been
-					                             // visited, put it into map and queue
-					visited.put(nei, neiId);
-					queue.offer(nei);
-				} else if (visited.get(nei) != neiId) { // if current neighbor has been visited
-					                                    // but it belongs to current node's group
-					                                    // then the graph is not bipartite
+		// run bfs on given node's neighbors
+		// the standard is to guarantee each neighbor has different groupID of
+		// current node
+		Queue<GraphNode> q = new LinkedList<>();
+		q.offer(node);
+		map.put(node, 1); // assume two groups in the graph, 1 and 2
+		while (!q.isEmpty()) {
+			GraphNode cur = q.poll();
+			for (GraphNode neighbor : cur.neighbors) {
+				if (!map.containsKey(neighbor)) {
+					int groupId = map.get(cur) == 1 ? 2 : 1; // make sure each neighbor of current node has different groupId from it
+					map.put(neighbor, groupId);
+					q.offer(neighbor);
+				} else if (map.get(neighbor) == map.get(cur)) { // violate the Bipartite rule				
 					return false;
 				}
-				// else the neighbor node has been visited and it belongs to right group
-				// then we do nothing
 			}
 		}
 		return true;
 	}
 }
+//public class Bipartite {
+//	public boolean isBipartite(List<GraphNode> graph) {
+//		if (graph == null || graph.isEmpty()) {
+//			return true;
+//		}
+//		// use a hash_map to record if a graphnode which belongs
+//		// to a certain group is visited or not
+//		// key: current graph node, value: the group it belongs to
+//		Map<GraphNode, Integer> visited = new HashMap<>();
+//		for (GraphNode node : graph) {
+//			// run bfs on each node of graph and check the validate
+//			// relationship between expanded node and its neighbors
+//			if (!bfs(node, visited)) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//	
+//	private boolean bfs(GraphNode node, Map<GraphNode, Integer> visited) {
+//		// if we have visited the node, we do not have to run bfs
+//		// on this node again, this prunes the duplicate operations
+//		if (visited.containsKey(node)) {
+//			return true;
+//		}
+//		// put the current node into random group, say, group 0
+//		// then all the neighbors of it shall belong to group 1
+//		// if the graph is bipartite
+//		visited.put(node, 0);
+//		// run bfs
+//		Queue<GraphNode> queue = new LinkedList<>();
+//		queue.offer(node);
+//		while (!queue.isEmpty()) {
+//			GraphNode cur = queue.poll();
+//			for (GraphNode nei : cur.neighbors) {
+//				int groupId = visited.get(cur);
+//				// the neighbors shall all belong to opposite
+//				// group of current node if the graph is bipartite
+//				int neiId = groupId == 0 ? 1 : 0;
+//				if (!visited.containsKey(nei)) { // if current neighbor of cur has not been
+//					                             // visited, put it into map and queue
+//					visited.put(nei, neiId);
+//					queue.offer(nei);
+//				} else if (visited.get(nei) != neiId) { // if current neighbor has been visited
+//					                                    // but it belongs to current node's group
+//					                                    // then the graph is not bipartite
+//					return false;
+//				}
+//				// else the neighbor node has been visited and it belongs to right group
+//				// then we do nothing
+//			}
+//		}
+//		return true;
+//	}
+//}
