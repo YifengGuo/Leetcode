9b6b279e7a8e4ca404e969ea49b2a6e6b0646200
YifengGuo <gyf940517@gmail.com>
2018-03-30 22:22:00 -0400

    more elegant method, remember index out of bounds

--- src/array/BinarySearch/KClosestInSortedArray.java
+++ src/array/BinarySearch/KClosestInSortedArray.java
@@ -20,49 +20,85 @@ A = {1, 4, 6, 8}, T = 3, K = 3, return {4, 1, 6}
 /*
  * time = O(Math.max(k, logn))
  */
+// more elegant way
 public class KClosestInSortedArray {
 	public int[] kClosest(int[] array, int target, int k) {
 		if (array == null || array.length == 0) {
-			return array;
+			return new int[0];
 		}
-		int start = 0;
-		int end = array.length - 1;
-		int[] res = new int[k];
-		while (end - start > 1) {
-			int mid = start + (end - start) / 2;
-			if (array[mid] >= target) {
-				end = mid;
+		int lo = 0;
+		int hi = array.length - 1;
+		while (hi - lo > 1) {
+			int mid = lo + (hi - lo) / 2;
+			if (array[mid] == target) {
+				hi = mid;
+			} else if (array[mid] > target) {
+				hi = mid;
 			} else {
-				start = mid;
+				lo = mid;
 			}
 		}
-		int i = 0;
-		while (i < k) { // iterate k closest
-			if (start < 0 || end > array.length - 1) { // start or end may be out of bound
-				if (start < 0) {
-					res[i++] = array[end++];
-					continue;
-				} else if (end > array.length - 1) {
-					res[i++] = array[start--];
-					continue;
+		int[] res = new int[k];
+		int index = 0;
+		for (int i = 0; i < k; i++) {
+			if (lo >= 0 && hi < array.length) {
+				if (Math.abs(array[lo] - target) < Math.abs(array[hi] - target)) {
+					res[index++] = array[lo--];
 				} else {
-					break;
+					res[index++] = array[hi++];
 				}
-			} else if (array[end] == target) {
-				res[i++] = array[end++];
-				continue;
-			} else if (array[start] == target) {
-				res[i++] = array[start--];
-				continue;
-			} else { // determine which one is closer to target
-				if (Math.abs(array[start] - target) < Math.abs(array[end] - target)) {
-					res[i++] = array[start--];
-				} else {
-					res[i++] = array[end++];
-				}
-				continue;
+			} else if (lo < 0) {
+				res[index++] = array[hi++];
+			} else {
+				res[index++] = array[lo--];
 			}
 		}
 		return res;
 	}
 }
+//public class KClosestInSortedArray {
+//	public int[] kClosest(int[] array, int target, int k) {
+//		if (array == null || array.length == 0) {
+//			return array;
+//		}
+//		int start = 0;
+//		int end = array.length - 1;
+//		int[] res = new int[k];
+//		while (end - start > 1) {
+//			int mid = start + (end - start) / 2;
+//			if (array[mid] >= target) {
+//				end = mid;
+//			} else {
+//				start = mid;
+//			}
+//		}
+//		int i = 0;
+//		while (i < k) { // iterate k closest
+//			if (start < 0 || end > array.length - 1) { // start or end may be out of bound
+//				if (start < 0) {
+//					res[i++] = array[end++];
+//					continue;
+//				} else if (end > array.length - 1) {
+//					res[i++] = array[start--];
+//					continue;
+//				} else {
+//					break;
+//				}
+//			} else if (array[end] == target) {
+//				res[i++] = array[end++];
+//				continue;
+//			} else if (array[start] == target) {
+//				res[i++] = array[start--];
+//				continue;
+//			} else { // determine which one is closer to target
+//				if (Math.abs(array[start] - target) < Math.abs(array[end] - target)) {
+//					res[i++] = array[start--];
+//				} else {
+//					res[i++] = array[end++];
+//				}
+//				continue;
+//			}
+//		}
+//		return res;
+//	}
+//}
